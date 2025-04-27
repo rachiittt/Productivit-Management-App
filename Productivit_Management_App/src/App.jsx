e13fc8bb35cbd873fb93d6069f28927bdee63aaa
Rachit Singh <rachit.singh.bbps.1@gmail.com>
2025-04-27 22:55:15 +0530

    imported everything in App.Jsx

--- Productivit_Management_App/src/App.jsx
+++ Productivit_Management_App/src/App.jsx
@@ -1,3 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import Navigation from './components/Navigation.jsx';
+import HomePage from './components/HomePage.jsx';
+import AboutPage from './components/AboutPage.jsx';
+import ServicesPage from './components/ServicesPage';
+import BlogPage from './components/BlogPage.jsx';
+import LoginSignupPage from './components/LoginSignupPage.jsx';
+import ProductivityFooter from './components/ProductivityFooter';
+import TodaysTasks from './components/TodaysTasks';
+import './index.css';
+
 function App() {
   
 }
